fbecc7650616b0ff721dc76b304f816086858e33
Yan César <65233281+Yan-pg@users.noreply.github.com>
2022-05-05 10:00:31 -0300

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,9 +7,9 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://instagram.fssa21-1.fna.fbcdn.net/v/t51.2885-15/e35/s480x480/42068846_547093959086702_3530168650571193204_n.jpg?tp=1&_nc_ht=instagram.fssa21-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Lf0B3t4LlYEAX82Wufj&oh=74124fd656a1d078e2a7a718b813d70a&oe=6063F934" alt="Yan César"/>
+            <img src="https://avatars.githubusercontent.com/u/65233281?v=4" alt="Yan César"/>
             <div>
-                <strong>TalitinhaPG</strong>
+                <strong>Yan César</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
                     Level {level}
